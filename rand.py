ac8f4a8a04654e5cd3d5497204b2adfb5d664a7f
Ananya Patankar <ananya09patankar@gmail.com>
2024-09-12 17:08:59 +0000

    Adding correct code changes for rand file

--- rand.py
+++ rand.py
@@ -1,7 +1,23 @@
+"""
+This module provides functions to generate random numbers.
+
+It includes:
+- `random_array(arr)`: Add numbers the given list with random integers between 1 and 100.
+"""
+
 import random
 
 def random_array(arr):
-    for i in range(len(arr)):
-        shuffled_num = random.randint(1,100)
-        arr[i] = shuffled_num
+    """
+    Populates the provided array with random integers between 1 and 100.
+
+    Parameters:
+    arr (list): A list to be filled with random integers.
+
+    Returns:
+    list: The list filled with random integers.
+    """
+    for ind, _ in enumerate(arr):
+        shuffled_num = random.randint(1, 100)
+        arr[ind] = shuffled_num
     return arr
